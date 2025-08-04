f684c7a6482b4a16f24c5967979cab0dfa0ef95a
praches <prachespani@gmail.com>
2025-08-05 01:22:27 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,13 @@
+# TASK-----1
+
+A = int(input("Enter the FIRST number:"))
+B = int(input("Enter the SECOND number:"))
+
+print("ADDITION:",A+B)
+
+print("SUBSTRACTION:",A-B)
+
+print("MULTIPLICATION:",A*B)
+
+print("DIVISION:",A/B)
+
---
+++ task2.py
@@ -0,0 +1,4 @@
+A = str(input(" Enter the FIRST name :"))
+B =str(input("Enter the LAST name :"))
+
+print("HELLO!\n" + A+B +" \nWELCOME TO THE PYTHON PROGRAMMING WORLD")
